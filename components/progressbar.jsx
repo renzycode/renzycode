77b91099384669aa7d5b9c3b949e0c86eb68f7ec
renzycode <renzyjohnm1@gmail.com>
2023-02-08 20:28:00 +0800

    update

---
+++ components/progressbar.jsx
@@ -0,0 +1,18 @@
+const Progressbar = (props) => {
+  return (
+    <div>
+      <div class="mb-1 text-lg font-medium text-black flex">{props.title}</div>
+      <div class="w-full h-4 mb-4 bg-gray-200 rounded-full dark:bg-gray-700">
+        <div
+          style={{ width: props.percent }}
+          class="bg-gradient-to-r dark:from-green-500 from-green-200 to-blue-400 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+        >
+          {" "}
+          {props.percent}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Progressbar;
